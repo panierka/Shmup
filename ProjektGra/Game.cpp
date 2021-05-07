47ae98c744613ec464bf2b85c948d05959389da0
W <w___B@wp.pl>
2021-05-07 18:14:05 +0200

    Zaimplenetowanie SFML

--- ProjektGra/Game.cpp
+++ ProjektGra/Game.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
 
-using namespace std;
+using namespace sf;
 
 int main()
 {
-	cout << "Test" << endl;
+	std::cout << "Test" << std::endl;
 
-	cout << "Test2" << endl;
-	cout << "Test4" << endl;
+	std::cout << "Test2" << std::endl;
+	std::cout << "Test4" << std::endl;
+
+	RenderWindow window(VideoMode(200, 200), "POG");
+	CircleShape kolo(50.0f);
+	kolo.setFillColor(Color::Red);
+
+	while (window.isOpen())
+	{
+		Event _event;
+		while (window.pollEvent(_event))
+		{
+			if (_event.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(kolo);
+		window.display();
+	}
 }
