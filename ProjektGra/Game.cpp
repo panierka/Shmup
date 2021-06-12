2085366d1b6b39aa14d26a03728e1517f7041752
W <w___B@wp.pl>
2021-06-13 00:34:37 +0200

    :O)

--- ProjektGra/Game.cpp
+++ ProjektGra/Game.cpp
@@ -96,12 +96,22 @@ int main()
 
 	// stworzenie okna gry
 	RenderWindow window(VideoMode(SCREEN_SIZE.x + 275u, SCREEN_SIZE.y), "Gra", Style::Titlebar | Style::Close);
+
 	MainMenu menu(SCREEN_SIZE.x + 275u, SCREEN_SIZE.y);
+
+	bool exit_menu = false;
+
 	while (window.isOpen())
 	{
 		Event _event;
+
 		while (window.pollEvent(_event))
 		{
+			if (_event.type == Event::Closed)
+			{
+				window.close();
+			}
+			
 			if (_event.type == Event::KeyReleased)
 			{
 				if (_event.key.code == Keyboard::PageDown)
@@ -114,107 +124,112 @@ int main()
 				}
 				if (_event.key.code == Keyboard::Enter)
 				{
-					// wczytanie tekstur gracza i stworzenie jego obiektu
-					Texture t, t2;
-					t.loadFromFile("../Assets/Player.png");
-					t2.loadFromFile("../Assets/Player-Projectile.png");
-
-#pragma region > Inicj. Tekstur <
-
-					update_texture_atlas("player", "Player");
-					update_texture_atlas("fly", "Fly");
-					update_texture_atlas("player-bullet", "Player-Projectile");
-					update_texture_atlas("enemy-bullet", "Enemy-Bullet");
-					update_texture_atlas("player", "Player");
-					update_texture_atlas("fly", "Fly");
-					update_texture_atlas("player-bullet", "Player-Projectile");
-					update_texture_atlas("enemy-bullet", "Enemy-Bullet");
-					update_texture_atlas("block1", "Block1");
-					update_texture_atlas("block2", "Block2");
-
-#pragma endregion
-
-
-					std::unique_ptr<Player> _player = make_unique<Player>((Vector2f)SCREEN_SIZE / 2.f + Vector2f(0, 375), generate_sprite(texture_atlas["player"], Vector2f(50.f, 50.f)), false, Vector2i(100, 100));
-
-					_player->animations.push_back(new AnimationClip(0, 4, 10, *_player, true));
-					_player->animations.push_back(new AnimationClip(5, 4, 18, *_player, false));
-					_player->animations.push_back(new AnimationClip(10, 3, 12, *_player, false));
-
-					_player->create_collider(Vector2f(0.f, 14.f), Vector2f(36.f, 40.f));
-
-					_player->add_max_health(60);
-					_player->add_max_ammo(6);
-
-					//_player->start();
-
-					Engine::objects.push_back(std::move(_player));
-					//player = *_player;
-
-					Texture t1, t3;
-
-					t1.loadFromFile("../Assets/Fly.png");
-					t3.loadFromFile("../Assets/Enemy-Bullet.png");
-
-					// inicjalizacja dodatkowych komponentów
-					InputHandler input(dynamic_cast<Player*>(Engine::objects[0].get()));
-					Engine engine(&window);
-					// inicjalizacja zmiennych do kalkulowania czasu miêdzy klatkami
-					Clock clock;
-					float _frame_time = clock.getElapsedTime().asSeconds();
-
-					//sound1.add_sound("pogchamp", "../Assets/Sounds/Soundtrack.wav", 20);
-
-						//sound1.play_sound("pogchamp");
-						/*BackgroundMusic background;*/
-					WaveSpawner::big_enemies.push_back(spawn_fly);
-
-					WaveSpawner::small_enemies.push_back(spawn_block1);
-					WaveSpawner::small_enemies.push_back(spawn_block2);
-
-					WaveSpawner wave;
-					wave.set_wave(15);
-
-
-					window.setFramerateLimit(60);
-					// sprawdzenie zdarzeñ okna
-					while (window.isOpen())
-					{
-						Event _event1;
-						while (window.pollEvent(_event1))
-						{
-							if (_event1.type == Event::Closed)
-							{
-								window.close();
-							}
-
-						}
-						// dynamiczne kalkulowanie realnego delta t miêdzy kolejnymi klatkami z "wyg³adzaniem"
-						_frame_time = clock.restart().asSeconds();
-
-
-						wave.spawn(_frame_time);
-
-						// sprawdzenie akcji gracza
-						input.check_input();
-
-						// wyœwietlanie poprawnych informacji na ekranie
-
-						engine.update(_frame_time);
-
-						// wykonanie siê obliczeñ czasomierzy
-						tick_timers(_frame_time);
-					}
+					exit_menu = true;
 				}
-
-
-
 			}
 		}
+
+		if (exit_menu)
+		{
+			break;
+		}
+
 		window.clear(Color(73, 84, 123, 255));
 		menu.print_menu(window);
 		window.display();
 	}
+
+	// wczytanie tekstur gracza i stworzenie jego obiektu
+	Texture t, t2;
+	t.loadFromFile("../Assets/Player.png");
+	t2.loadFromFile("../Assets/Player-Projectile.png");
+
+#pragma region > Inicj. Tekstur <
+
+	update_texture_atlas("player", "Player");
+	update_texture_atlas("fly", "Fly");
+	update_texture_atlas("player-bullet", "Player-Projectile");
+	update_texture_atlas("enemy-bullet", "Enemy-Bullet");
+	update_texture_atlas("player", "Player");
+	update_texture_atlas("fly", "Fly");
+	update_texture_atlas("player-bullet", "Player-Projectile");
+	update_texture_atlas("enemy-bullet", "Enemy-Bullet");
+	update_texture_atlas("block1", "Block1");
+	update_texture_atlas("block2", "Block2");
+
+#pragma endregion
+
+
+	std::unique_ptr<Player> _player = make_unique<Player>((Vector2f)SCREEN_SIZE / 2.f + Vector2f(0, 375), generate_sprite(texture_atlas["player"], Vector2f(50.f, 50.f)), false, Vector2i(100, 100));
+
+	_player->animations.push_back(new AnimationClip(0, 4, 10, *_player, true));
+	_player->animations.push_back(new AnimationClip(5, 4, 18, *_player, false));
+	_player->animations.push_back(new AnimationClip(10, 3, 12, *_player, false));
+
+	_player->create_collider(Vector2f(0.f, 14.f), Vector2f(36.f, 40.f));
+
+	_player->add_max_health(60);
+	_player->add_max_ammo(6);
+
+	//_player->start();
+
+	Engine::objects.push_back(std::move(_player));
+	//player = *_player;
+
+	Texture t1, t3;
+
+	t1.loadFromFile("../Assets/Fly.png");
+	t3.loadFromFile("../Assets/Enemy-Bullet.png");
+
+	// inicjalizacja dodatkowych komponentów
+	InputHandler input(dynamic_cast<Player*>(Engine::objects[0].get()));
+	Engine engine(&window);
+	// inicjalizacja zmiennych do kalkulowania czasu miêdzy klatkami
+	Clock clock;
+	float _frame_time = clock.getElapsedTime().asSeconds();
+
+	//sound1.add_sound("pogchamp", "../Assets/Sounds/Soundtrack.wav", 20);
+
+		//sound1.play_sound("pogchamp");
+		/*BackgroundMusic background;*/
+	WaveSpawner::big_enemies.push_back(spawn_fly);
+
+	WaveSpawner::small_enemies.push_back(spawn_block1);
+	WaveSpawner::small_enemies.push_back(spawn_block2);
+
+	WaveSpawner wave;
+	wave.set_wave(15);
+
+
+	window.setFramerateLimit(60);
+	// sprawdzenie zdarzeñ okna
+	while (window.isOpen())
+	{
+		Event _event;
+		while (window.pollEvent(_event))
+		{
+			if (_event.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+		// dynamiczne kalkulowanie realnego delta t miêdzy kolejnymi klatkami z "wyg³adzaniem"
+		_frame_time = clock.restart().asSeconds();
+
+
+		wave.spawn(_frame_time);
+
+		// sprawdzenie akcji gracza
+		input.check_input();
+
+		// wyœwietlanie poprawnych informacji na ekranie
+
+		engine.update(_frame_time);
+
+		// wykonanie siê obliczeñ czasomierzy
+		tick_timers(_frame_time);
+	}
+
 	/*for (std::size_t i = 0; i < Engine::objects.size(); i++)
 	{
 		Engine::objects[i].reset();
